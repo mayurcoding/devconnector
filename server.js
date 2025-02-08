a235caabb2ef28cadb498d97c46ea4d90c085a3c
Mayur Rastogi <mayurrastogi46@gmail.com>
2025-02-08 15:42:07 +0530

    change to sever js file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) =>  res.send('Mayur Rastogi 7454955886'));
+app.get('/', (req, res) =>  res.send('Heloo Mayur !'));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
