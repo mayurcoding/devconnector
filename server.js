c41ec10f9afb33da4de439fe04baf7c5db083898
Mayur Rastogi <mayurrastogi46@gmail.com>
2025-02-09 14:34:25 +0530

    connect to mongodb

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connnect to MongoDB
+mongoose.connect(db)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) =>  res.send('Heloo Mayur !'));
 const port = process.env.PORT || 5000;
