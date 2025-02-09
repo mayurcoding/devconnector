3d99b418306bcfb3cfeae35b46c23d18059ffa03
Mayur Rastogi <mayurrastogi46@gmail.com>
2025-02-09 15:31:01 +0530

    change some settings

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const router = express.Router();`
+const router = express.Router();
 
 router.get('/test', (req, res) => res.json({msg: "Users Works"}));
 
 module.exports = router;    
-`
