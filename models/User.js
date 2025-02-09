9370cab72faf7b7a7688113d66067fa66f817e26
Mayur Rastogi <mayurrastogi46@gmail.com>
2025-02-09 15:50:04 +0530

    added models

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const scheme = mongoose.Schema;
+
+//  const scheme
+const UserScheme = new scheme({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongoose.model('users', UserScheme);
